aba7995fb50fade9edbee5c467fafbcfec024205
Jorge Xinico <jxinicob@miumg.edu.gt>
2023-01-12 16:47:15 -0600

    pruebas

---
+++ controllers/ProductoController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Controllers;
+use MVC\Router;
+
+class ProductoController{
+    public static function index(Router $router){
+        $router->render('productos/index', []);
+    }
+}
+?>
